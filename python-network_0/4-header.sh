401f78f726ade48a34d5fb4d179798432e0fa9c4
uwase ndegeya nina <u.ndegeya@alustudent.com>
2026-08-06 19:42:17 +0000

    edited 4-header.sh file

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with the required custom header
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with the required header
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
