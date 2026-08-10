fa3d9d5032e2579c386d5b79b8a74dd99d408579
NDEGEYA Nina UWASE <u.ndegeya@alustudent.com>
2026-08-10 21:28:11 +0200

    completed python almost a circle

--- python-almost_a_circle/6-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Test Rectangle display with offsets."""
-from models.rectangle import Rectangle
-
-if __name__ == "__main__":
-    Rectangle(2, 3, 2, 2).display()
-    print("---")
-    Rectangle(3, 2, 1, 0).display()
